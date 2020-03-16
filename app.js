7593b780a2e4b8a4ecd2e728d6ce8c881cae780a
Yash Jain <31776391+YashJainHSR@users.noreply.github.com>
2020-03-16 15:36:35 +0530

    weather-app-nodejs-commandline

---
+++ app.js
@@ -0,0 +1,20 @@
+const geocode = require('./geocode');
+const forecast = require('./forecast');
+
+const address = process.argv[2];
+if(!address) {
+    return console.log('Please provide an address!');
+}
+
+geocode(address, (error, {location, latitude, longitude}) => {
+    if(error) {
+        return console.log(error);
+    }
+    console.log(location);
+    forecast(latitude, longitude, (error, {summary, temperature, precipProbability}) => {
+        if(error) {
+            return console.log(error);
+        }
+        console.log(summary + ' It is currently ' + temperature + ' degrees out. There is a ' + precipProbability + '% chance of rain');
+    });
+});
---
+++ forecast.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const forecast = (latitude, longitude , callback) => {
+    const url = 'https://api.darksky.net/forecast/892e768ab477cbc3833b72d8e08de072/'+ latitude +','+ longitude +'?units=si';
+
+    request({url, json: true}, (error, {body}) => {
+        if(error) {
+            callback('Unable to connect to weather services!', undefined); 
+        } else if (body.error) {
+            callback('Unable to find location!', undefined); 
+        } else {
+            callback(undefined, {
+                summary: body.daily.data[0].summary,
+                temperature: body.currently.temperature,
+                precipProbability: body.currently.precipProbability
+            });
+        }
+    });
+}
+
+module.exports = forecast;
